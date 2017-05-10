36c231cb9274d66771740acefa45e26379500ad3
GrizzlyJones <j.hornholt@gmail.com>
2017-05-10 17:08:17 +0200

    Finishing touches, including added comments

--- scripts/freeboardAnalysis.m
+++ scripts/freeboardAnalysis.m
@@ -1,53 +1,70 @@
 function iceSheets = freeboardAnalysis(fluxgate)
+%FREEBOARDANALYSIS Splits data between leads in the fluxgate
+%   iceSheets = freeboardAnalysis(FLUXGATE) returns a struct with freeboard
+%   heights between leads. The freeboard height is calculated as the height
+%   of the surface minus the average lead height surrounding the ice. Some
+%   sheets of ice are filtered, due to lack of data.
+%   The filtration happens if the icesheet has a width of two data points
+%   or less, due to insufficient data. It is also filtrated if a NaN value
+%   is pressent, due to NaN values being a mask for bodies of water.
+%
+%   See also INITFLUXGATE
+
+%% Argument checking
+if length(fluxgate.sla) ~= length(fluxgate.class)
+    error('Error:\nDimensions don\'' match');
+end
+
+%% Generate structs
 iceSheets = struct('lon', [], 'lat', [], 'freeboard', [], 'index', []);
 iceSheets.index = struct('start', [], 'stop', []);
 
 tmp = struct('heights', [], 'leads', []);
 
-lon = fluxgate.lon;
-lat = fluxgate.lat;
-heights = fluxgate.sla;
-leads = fluxgate.class;
-
-if length(heights) ~= length(leads)
-    error('Error:\nDimensions don\'' match');
-end
-
-N = length(heights);
-
+%% init meta data
 initial = true;
 newFreeboard = false;
 index = 1;
 indexStop = nan;
 
-for i = 1:N
-    if leads(i) == 4 && ~newFreeboard
+%% Lead and ice sheet detection
+for i = 1:length(fluxgate.sla)
+    % if value is a lead and it's a new ice sheet
+    if fluxgate.class(i) == 4 && ~newFreeboard
         tmpStop = indexStop;
         indexStop = i - 1;
         newFreeboard = true;
         
+        % skips the first lead, due to lack of starting lead
         if ~initial
-            if any(isnan(heights(indexStart:indexStop))) || ((indexStop - indexStart) < 3)
+            % filter, see header for more info
+            if any(isnan(fluxgate.sla(indexStart:indexStop))) || ...
+                    ((indexStop - indexStart) < 3)
                 continue;
             end
-            iceSheets(index).lon = lon(indexStart:indexStop);
-            iceSheets(index).lat = lat(indexStart:indexStop);
+            
+            % ice sheet extractin
+            iceSheets(index).lon = fluxgate.lon(indexStart:indexStop);
+            iceSheets(index).lat = fluxgate.lat(indexStart:indexStop);
             iceSheets(index).index.start = indexStart;
             iceSheets(index).index.stop = indexStop;
             
-            tmp(index).heights = heights(indexStart:indexStop);
-            tmp(index).leads = heights(tmpStop:indexStart);
+            tmp(index).heights = fluxgate.sla(indexStart:indexStop);
+            tmp(index).leads = fluxgate.sla(tmpStop:indexStart);
             
             index = index + 1;
         end
+    % if value is part of a new ice sheet
     elseif newFreeboard
         indexStart = i;
         newFreeboard = false;
         initial = false;
     end
 end
-tmp(index).leads = heights(tmpStop:indexStart);
+% Includes last leads data
+tmp(index).leads = fluxgate.sla(tmpStop:indexStart);
 
+%% Freeboard calculations
 for i = 1:length(iceSheets)
     avgLead = (mean(tmp(i).leads) + mean(tmp(i+1).leads)) / 2;
     iceSheets(i).freeboard = tmp(i).heights - avgLead;

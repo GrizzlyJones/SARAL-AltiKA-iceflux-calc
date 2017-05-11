f5c57887056f3c13df515e46d91befdf860882bd
FillClinton <filipfloks@hotmail.com>
2017-05-11 15:56:34 +0200

    Added bearing file to velocity

--- scripts/velocity.m
+++ scripts/velocity.m
@@ -1,10 +1,10 @@
 function [t] = velocity(fileName)
 % Indhentning af hastighederne fra www.seaice.dk/N/YYYY/MM/DD/
 
-t = struct('lon', [], 'lat', [], 'x', [], 'y', []);
+t = struct('lon', [], 'lat', [], 'x', [], 'y', [], 'magn', [], 'brng', []);
 
 fileID = fopen(fileName);
-M = textscan(fileID, '%f %f %f %f %s ', 'Headerlines', 1);
+M = textscan(fileID, '%f %f %f %f %s', 'Headerlines', 1);
 fclose(fileID);
 
 A = cell2mat(M(1:4));
@@ -17,10 +17,12 @@ lat = A(isnan(skip),1);
 t.lon = lon(start == 3);
 t.lat = lat(start == 1);
 
-t.x = coor2dist(lon(start == 1), lat(start == 3), t.lon, t.lat);
-t.y = coor2dist(lon(start == 1), t.lat, t.lon, lat(start == 3)); 
+t.x = coor2dist(lon(start == 1), lat(start == 3), t.lon, lat(start == 3));
+t.y = coor2dist(lon(start == 1), t.lat, lon(start == 1), lat(start == 3)); 
 
+t.brng = coor2brng(lon(start == 1), t.lat, t.lon, lat(start == 3));
 
+t.magn = coor2dist(lon(start == 1), t.lat, t.lon, lat(start == 3));  
 
 % B = cell2mat(M(4)); 
 % C = celldisp(M(5));

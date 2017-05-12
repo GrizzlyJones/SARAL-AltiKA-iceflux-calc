b209fe3a8268fff67dc64549df8235d931a2647d
FillClinton <filipfloks@hotmail.com>
2017-05-12 16:55:57 +0200

    lon and lat changes

--- scripts/velocity.m
+++ scripts/velocity.m
@@ -8,25 +8,25 @@ M = textscan(fileID, '%f %f %f %f %s', 'Headerlines', 1);
 fclose(fileID);
 
 A = cell2mat(M(1:4));
-skip = A(:,4);
-start = A(isnan(skip),3);
+skip = isnan(A(:,4));
+start = A(skip,3);
 
-lon = A(isnan(skip),2);
-lat = A(isnan(skip),1);
+lon = A(skip,2);
+lat = A(skip,1);
 
 t.lon = lon(start == 3);
-t.lat = lat(start == 1);
+t.lat = lat(start == 3);
 
-t.x = coor2dist(lon(start == 1), lat(start == 3), t.lon, lat(start == 3));
-t.y = coor2dist(lon(start == 1), t.lat, lon(start == 1), lat(start == 3)); 
+t.x = coor2dist(lon(start == 1), lat(start == 3), lon(start == 3), lat(start == 3));
+t.y = coor2dist(lon(start == 1), lat(start == 1), lon(start == 1), lat(start == 3)); 
 
 t.xy = sqrt(t.x.^2 + t.y.^2);
-t.XY = coor2dist(lon(start == 1), t.lat, t.lon, lat(start == 3)); 
+t.XY = coor2dist(lon(start == 1), lat(start == 1), lon(start == 3), lat(start == 3)); 
 
 
-t.brng = coor2brng(lon(start == 1), t.lat, t.lon, lat(start == 3));
+t.brng = coor2brng(lon(start == 1), lat(start == 1), lon(start == 3), lat(start == 3));
 
-t.magn = coor2dist(lon(start == 1), t.lat, t.lon, lat(start == 3));
+t.magn = coor2dist(lon(start == 1), lat(start == 1), lon(start == 3), lat(start == 3));
 
 % B = cell2mat(M(4)); 
 % C = celldisp(M(5));

003dc568e8bc016758f97d3a8a5557512f54c7aa
GrizzlyJones <j.hornholt@gmail.com>
2017-05-15 12:44:25 +0200

    fixed paramter placement

--- scripts/thickness.m
+++ scripts/thickness.m
@@ -1,9 +1,5 @@
 function [T] = thickness(freeboard)
-
 % Sea ice thickness
-
-
-T = freeboard .* rho_w/(rho_w - rho_i) + Z * (rho_s-rho_w)/(rho_w-rho_i) + P * rho_w/(rho_w-rho_i);
 
 % IceBridge L4 Sea Ice Freeboard, Snow Depth, and Thickness giver nogle
 % andre værdier for nedenstående konstanter. 
@@ -13,5 +9,6 @@ rho_s = 300;    % Density of snow in kg/m^3
 Z = 0.3;        % Snow height from ESA cryosat2
 P = 0.15;       % Penetration from ESA cryosat2
 
+T = freeboard .* rho_w/(rho_w - rho_i) + Z * (rho_s-rho_w)/(rho_w-rho_i) + P * rho_w/(rho_w-rho_i);
 
 end
